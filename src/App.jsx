d6b00defe174e6760b06ade71dbdefc5d24fc2eb
ankitjhagithub21 <ajha45075@gmail.com>
2025-04-04 17:01:39 +0530

    fix song list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Sidebar from "./components/Sidebar"
 import SongList from "./components/SongList"
 import Player from "./components/Player"
-import { useSelector } from "react-redux"
+
 
 
 const App = () => {
-  const {currentSong} = useSelector(state=>state.song)
+ 
   return (
     <main className="main-container">
       <div className="row">
